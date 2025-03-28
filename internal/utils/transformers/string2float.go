e5fdf88e4f827812622df34cb7601edd82e3a0ef
Raffaello De Pieri <raffaello.depieri@onefootball.com>
2025-03-28 12:41:14 +0000

    test(utils): add test coverage for utils
    new coverage over 80.0% of statements, min is 84.3, max is 100.0
    
    Signed-off-by: Raffaello De Pieri <raffaello.depieri@onefootball.com>

--- internal/utils/transformers/string2float.go
+++ internal/utils/transformers/string2float.go
@@ -3,10 +3,15 @@ package transformers
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 func String2Float64(s string) (float64, error) {
-	parsed, err := strconv.ParseFloat(s, 64)
+	if s == "" {
+		return 0, fmt.Errorf("input string is empty")
+	}
+
+	parsed, err := strconv.ParseFloat(strings.TrimSpace(s), 64)
 	if err != nil {
 		return 0, fmt.Errorf("failed to parse string to float64: %v", err)
 	}
