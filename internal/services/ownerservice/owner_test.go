fd8a03d7134b3d419fc2e3b7d246531576295e21
Raghav Tandon <raghavtan@gmail.com>
2025-06-12 14:40:11 +0200

    fix: update ownerservice tests
    -
    Signed-off-by: Raghav Tandon <raghavtan@gmail.com>
     
    -
    Signed-off-by: Raghav Tandon <raghavtan@gmail.com>

--- internal/services/ownerservice/owner_test.go
+++ internal/services/ownerservice/owner_test.go
@@ -1,7 +1,6 @@
 package ownerservice_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/motain/of-catalog/internal/services/ownerservice"
@@ -9,83 +8,286 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func mockGetSquadDetails(squadName string) (ownerservice.SquadDetails, error) {
-	mockData := map[string]ownerservice.SquadDetails{
+// createMockSquads creates a map of mock Group structs for testing
+func createMockSquads() map[string]ownerservice.Group {
+	return map[string]ownerservice.Group{
 		"squad1": {
-			JiraTeamID:      "ari:cloud:identity::team/squad1",
-			SlackURL:        "https://onefootball.slack.com/archives/FOOBAR",
-			SlackTitle:      "squad1-chat",
-			JiraProjectURL:  "https://onefootball.atlassian.net/jira/software/projects/ITP/boards/1278",
-			JiraProjectName: "IT Projects",
-			Tribe:           "TRIBE FOOBARBZ42",
+			APIVersion: "backstage.io/v1alpha1",
+			Kind:       "Group",
+			Metadata: struct {
+				Name        string            `yaml:"name"`
+				Description string            `yaml:"description"`
+				Annotations map[string]string `yaml:"annotations"`
+				Links       []struct {
+					Title string `yaml:"title"`
+					URL   string `yaml:"url"`
+					Icon  string `yaml:"icon"`
+					Type  string `yaml:"type"`
+				} `yaml:"links"`
+			}{
+				Name:        "squad1",
+				Description: "Test squad 1",
+				Annotations: map[string]string{
+					"jiraTeamID": "ari:cloud:identity::team/squad1",
+				},
+				Links: []struct {
+					Title string `yaml:"title"`
+					URL   string `yaml:"url"`
+					Icon  string `yaml:"icon"`
+					Type  string `yaml:"type"`
+				}{
+					{
+						Title: "squad1-chat",
+						URL:   "https://onefootball.slack.com/archives/FOOBAR",
+						Type:  "slack",
+					},
+					{
+						Title: "IT Projects",
+						URL:   "https://onefootball.atlassian.net/jira/software/projects/ITP/boards/1278",
+						Icon:  "jira",
+						Type:  "project",
+					},
+				},
+			},
+			Spec: struct {
+				Profile struct {
+					DisplayName string `yaml:"displayName"`
+				} `yaml:"profile"`
+				Type     string   `yaml:"type"`
+				Parent   string   `yaml:"parent"`
+				Children []string `yaml:"children"`
+			}{
+				Type:   "squad",
+				Parent: "TRIBE FOOBARBZ42",
+			},
 		},
 		"squad-no-slack": {
-			JiraTeamID:      "ari:cloud:identity::team/squad-no-slack",
-			SlackURL:        "",
-			SlackTitle:      "",
-			JiraProjectURL:  "https://onefootball.atlassian.net/jira/software/projects/SP/boards/123",
-			JiraProjectName: "Some Project",
-			Tribe:           "TRIBE FOOBARBZ42",
+			APIVersion: "backstage.io/v1alpha1",
+			Kind:       "Group",
+			Metadata: struct {
+				Name        string            `yaml:"name"`
+				Description string            `yaml:"description"`
+				Annotations map[string]string `yaml:"annotations"`
+				Links       []struct {
+					Title string `yaml:"title"`
+					URL   string `yaml:"url"`
+					Icon  string `yaml:"icon"`
+					Type  string `yaml:"type"`
+				} `yaml:"links"`
+			}{
+				Name:        "squad-no-slack",
+				Description: "Test squad without slack",
+				Annotations: map[string]string{
+					"jiraTeamID": "ari:cloud:identity::team/squad-no-slack",
+				},
+				Links: []struct {
+					Title string `yaml:"title"`
+					URL   string `yaml:"url"`
+					Icon  string `yaml:"icon"`
+					Type  string `yaml:"type"`
+				}{
+					{
+						Title: "Some Project",
+						URL:   "https://onefootball.atlassian.net/jira/software/projects/SP/boards/123",
+						Icon:  "jira",
+						Type:  "project",
+					},
+				},
+			},
+			Spec: struct {
+				Profile struct {
+					DisplayName string `yaml:"displayName"`
+				} `yaml:"profile"`
+				Type     string   `yaml:"type"`
+				Parent   string   `yaml:"parent"`
+				Children []string `yaml:"children"`
+			}{
+				Type:   "squad",
+				Parent: "TRIBE FOOBARBZ42",
+			},
 		},
 		"squad-no-projects": {
-			JiraTeamID:      "ari:cloud:identity::team/squad-no-projects",
-			SlackURL:        "https://onefootball.slack.com/archives/CHANNEL",
-			SlackTitle:      "squad-chat",
-			JiraProjectURL:  "",
-			JiraProjectName: "",
-			Tribe:           "TRIBE FOOBARBZ42",
+			APIVersion: "backstage.io/v1alpha1",
+			Kind:       "Group",
+			Metadata: struct {
+				Name        string            `yaml:"name"`
+				Description string            `yaml:"description"`
+				Annotations map[string]string `yaml:"annotations"`
+				Links       []struct {
+					Title string `yaml:"title"`
+					URL   string `yaml:"url"`
+					Icon  string `yaml:"icon"`
+					Type  string `yaml:"type"`
+				} `yaml:"links"`
+			}{
+				Name:        "squad-no-projects",
+				Description: "Test squad without projects",
+				Annotations: map[string]string{
+					"jiraTeamID": "ari:cloud:identity::team/squad-no-projects",
+				},
+				Links: []struct {
+					Title string `yaml:"title"`
+					URL   string `yaml:"url"`
+					Icon  string `yaml:"icon"`
+					Type  string `yaml:"type"`
+				}{
+					{
+						Title: "squad-chat",
+						URL:   "https://onefootball.slack.com/archives/CHANNEL",
+						Type:  "slack",
+					},
+				},
+			},
+			Spec: struct {
+				Profile struct {
+					DisplayName string `yaml:"displayName"`
+				} `yaml:"profile"`
+				Type     string   `yaml:"type"`
+				Parent   string   `yaml:"parent"`
+				Children []string `yaml:"children"`
+			}{
+				Type:   "squad",
+				Parent: "TRIBE FOOBARBZ42",
+			},
 		},
 		"minimal-squad": {
-			JiraTeamID:      "ari:cloud:identity::team/minimal-squad",
-			SlackURL:        "",
-			SlackTitle:      "",
-			JiraProjectURL:  "",
-			JiraProjectName: "",
-			Tribe:           "TRIBE FOOBARBZ42",
+			APIVersion: "backstage.io/v1alpha1",
+			Kind:       "Group",
+			Metadata: struct {
+				Name        string            `yaml:"name"`
+				Description string            `yaml:"description"`
+				Annotations map[string]string `yaml:"annotations"`
+				Links       []struct {
+					Title string `yaml:"title"`
+					URL   string `yaml:"url"`
+					Icon  string `yaml:"icon"`
+					Type  string `yaml:"type"`
+				} `yaml:"links"`
+			}{
+				Name:        "minimal-squad",
+				Description: "Minimal test squad",
+				Annotations: map[string]string{
+					"jiraTeamID": "ari:cloud:identity::team/minimal-squad",
+				},
+			},
+			Spec: struct {
+				Profile struct {
+					DisplayName string `yaml:"displayName"`
+				} `yaml:"profile"`
+				Type     string   `yaml:"type"`
+				Parent   string   `yaml:"parent"`
+				Children []string `yaml:"children"`
+			}{
+				Type:   "squad",
+				Parent: "TRIBE FOOBARBZ42",
+			},
 		},
 		"no-jira-team": {
-			JiraTeamID:      "",
-			SlackURL:        "https://onefootball.slack.com/archives/NOJIRA",
-			SlackTitle:      "no-jira-chat",
-			JiraProjectURL:  "",
-			JiraProjectName: "",
-			Tribe:           "TRIBE FOOBARBZ42",
+			APIVersion: "backstage.io/v1alpha1",
+			Kind:       "Group",
+			Metadata: struct {
+				Name        string            `yaml:"name"`
+				Description string            `yaml:"description"`
+				Annotations map[string]string `yaml:"annotations"`
+				Links       []struct {
+					Title string `yaml:"title"`
+					URL   string `yaml:"url"`
+					Icon  string `yaml:"icon"`
+					Type  string `yaml:"type"`
+				} `yaml:"links"`
+			}{
+				Name:        "no-jira-team",
+				Description: "Squad without jira team",
+				Links: []struct {
+					Title string `yaml:"title"`
+					URL   string `yaml:"url"`
+					Icon  string `yaml:"icon"`
+					Type  string `yaml:"type"`
+				}{
+					{
+						Title: "no-jira-chat",
+						URL:   "https://onefootball.slack.com/archives/NOJIRA",
+						Type:  "slack",
+					},
+				},
+			},
+			Spec: struct {
+				Profile struct {
+					DisplayName string `yaml:"displayName"`
+				} `yaml:"profile"`
+				Type     string   `yaml:"type"`
+				Parent   string   `yaml:"parent"`
+				Children []string `yaml:"children"`
+			}{
+				Type:   "squad",
+				Parent: "TRIBE FOOBARBZ42",
+			},
 		},
 		"different-tribe-squad": {
-			JiraTeamID:      "ari:cloud:identity::team/different-tribe-squad",
-			SlackURL:        "https://onefootball.slack.com/archives/DIFFERENT",
-			SlackTitle:      "different-squad-chat",
-			JiraProjectURL:  "https://onefootball.atlassian.net/jira/software/projects/DIFF/boards/999",
-			JiraProjectName: "Different Project",
-			Tribe:           "DIFFERENT TRIBE",
+			APIVersion: "backstage.io/v1alpha1",
+			Kind:       "Group",
+			Metadata: struct {
+				Name        string            `yaml:"name"`
+				Description string            `yaml:"description"`
+				Annotations map[string]string `yaml:"annotations"`
+				Links       []struct {
+					Title string `yaml:"title"`
+					URL   string `yaml:"url"`
+					Icon  string `yaml:"icon"`
+					Type  string `yaml:"type"`
+				} `yaml:"links"`
+			}{
+				Name:        "different-tribe-squad",
+				Description: "Squad from different tribe",
+				Annotations: map[string]string{
+					"jiraTeamID": "ari:cloud:identity::team/different-tribe-squad",
+				},
+				Links: []struct {
+					Title string `yaml:"title"`
+					URL   string `yaml:"url"`
+					Icon  string `yaml:"icon"`
+					Type  string `yaml:"type"`
+				}{
+					{
+						Title: "different-squad-chat",
+						URL:   "https://onefootball.slack.com/archives/DIFFERENT",
+						Type:  "slack",
+					},
+					{
+						Title: "Different Project",
+						URL:   "https://onefootball.atlassian.net/jira/software/projects/DIFF/boards/999",
+						Icon:  "jira",
+						Type:  "project",
+					},
+				},
+			},
+			Spec: struct {
+				Profile struct {
+					DisplayName string `yaml:"displayName"`
+				} `yaml:"profile"`
+				Type     string   `yaml:"type"`
+				Parent   string   `yaml:"parent"`
+				Children []string `yaml:"children"`
+			}{
+				Type:   "squad",
+				Parent: "DIFFERENT TRIBE",
+			},
 		},
 	}
-
-	if details, exists := mockData[squadName]; exists {
-		return details, nil
-	}
-	return ownerservice.SquadDetails{}, fmt.Errorf("squad '%s' not found", squadName)
-}
-
-func mockGetSquadDetailsWithError(squadName string) (ownerservice.SquadDetails, error) {
-	return ownerservice.SquadDetails{}, fmt.Errorf("failed to fetch squad details for '%s'", squadName)
 }
 
 func TestOwnerService_GetOwnerByTribeAndSquad(t *testing.T) {
 	tests := []struct {
-		name               string
-		tribe              string
-		squad              string
-		mockFunc           func(string) (ownerservice.SquadDetails, error)
-		expectedError      string
-		expectedData       *dtos.Owner
-		shouldContainError bool
+		name          string
+		tribe         string
+		squad         string
+		expectedError string
+		expectedData  *dtos.Owner
 	}{
 		{
-			name:     "successfully gets owner data with all fields",
-			tribe:    "TRIBE FOOBARBZ42",
-			squad:    "squad1",
-			mockFunc: mockGetSquadDetails,
+			name:  "successfully gets owner data with all fields",
+			tribe: "TRIBE FOOBARBZ42",
+			squad: "squad1",
 			expectedData: &dtos.Owner{
 				OwnerID: "ari:cloud:identity::team/squad1",
 				SlackChannels: map[string]string{
@@ -101,14 +303,12 @@ func TestOwnerService_GetOwnerByTribeAndSquad(t *testing.T) {
 			name:          "returns error when squad not found",
 			tribe:         "TRIBE FOOBARBZ42",
 			squad:         "nonexistent-squad",
-			mockFunc:      mockGetSquadDetails,
 			expectedError: "squad 'nonexistent-squad' not found",
 		},
 		{
-			name:     "handles squad with no Slack channel",
-			tribe:    "TRIBE FOOBARBZ42",
-			squad:    "squad-no-slack",
-			mockFunc: mockGetSquadDetails,
+			name:  "handles squad with no Slack channel",
+			tribe: "TRIBE FOOBARBZ42",
+			squad: "squad-no-slack",
 			expectedData: &dtos.Owner{
 				OwnerID:       "ari:cloud:identity::team/squad-no-slack",
 				SlackChannels: map[string]string{},
@@ -119,10 +319,9 @@ func TestOwnerService_GetOwnerByTribeAndSquad(t *testing.T) {
 			},
 		},
 		{
-			name:     "handles squad with no projects",
-			tribe:    "TRIBE FOOBARBZ42",
-			squad:    "squad-no-projects",
-			mockFunc: mockGetSquadDetails,
+			name:  "handles squad with no projects",
+			tribe: "TRIBE FOOBARBZ42",
+			squad: "squad-no-projects",
 			expectedData: &dtos.Owner{
 				OwnerID: "ari:cloud:identity::team/squad-no-projects",
 				SlackChannels: map[string]string{
@@ -133,10 +332,9 @@ func TestOwnerService_GetOwnerByTribeAndSquad(t *testing.T) {
 			},
 		},
 		{
-			name:     "handles squad with minimal data",
-			tribe:    "TRIBE FOOBARBZ42",
-			squad:    "minimal-squad",
-			mockFunc: mockGetSquadDetails,
+			name:  "handles squad with minimal data",
+			tribe: "TRIBE FOOBARBZ42",
+			squad: "minimal-squad",
 			expectedData: &dtos.Owner{
 				OwnerID:       "ari:cloud:identity::team/minimal-squad",
 				SlackChannels: map[string]string{},
@@ -145,10 +343,9 @@ func TestOwnerService_GetOwnerByTribeAndSquad(t *testing.T) {
 			},
 		},
 		{
-			name:     "handles squad with empty Jira team ID",
-			tribe:    "TRIBE FOOBARBZ42",
-			squad:    "no-jira-team",
-			mockFunc: mockGetSquadDetails,
+			name:  "handles squad with empty Jira team ID",
+			tribe: "TRIBE FOOBARBZ42",
+			squad: "no-jira-team",
 			expectedData: &dtos.Owner{
 				OwnerID: "",
 				SlackChannels: map[string]string{
@@ -159,10 +356,9 @@ func TestOwnerService_GetOwnerByTribeAndSquad(t *testing.T) {
 			},
 		},
 		{
-			name:     "successfully gets owner data for different tribe",
-			tribe:    "DIFFERENT TRIBE",
-			squad:    "different-tribe-squad",
-			mockFunc: mockGetSquadDetails,
+			name:  "successfully gets owner data for different tribe",
+			tribe: "DIFFERENT TRIBE",
+			squad: "different-tribe-squad",
 			expectedData: &dtos.Owner{
 				OwnerID: "ari:cloud:identity::team/different-tribe-squad",
 				SlackChannels: map[string]string{
@@ -175,25 +371,19 @@ func TestOwnerService_GetOwnerByTribeAndSquad(t *testing.T) {
 			},
 		},
 		{
-			name:          "handles GetSquadDetails error",
-			tribe:         "TRIBE FOOBARBZ42",
-			squad:         "any-squad",
-			mockFunc:      mockGetSquadDetailsWithError,
-			expectedError: "failed to fetch squad details for 'any-squad'",
-		},
-		{
 			name:          "handles empty squad parameter",
 			tribe:         "TRIBE FOOBARBZ42",
 			squad:         "",
-			mockFunc:      mockGetSquadDetails,
 			expectedError: "squad '' not found",
 		},
 	}
 
+	// Create service with mock squads
+	mockSquads := createMockSquads()
+	service := ownerservice.NewOwnerServiceWithSquads(mockSquads)
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			// Use dependency injection to provide the mock function
-			service := ownerservice.NewOwnerServiceWithDependencies(tt.mockFunc)
 			data, err := service.GetOwnerByTribeAndSquad(tt.tribe, tt.squad)
 
 			if tt.expectedError != "" {
@@ -212,7 +402,8 @@ func TestOwnerService_GetOwnerByTribeAndSquad_Interface(t *testing.T) {
 	// Test that OwnerService implements OwnerServiceInterface
 	var _ ownerservice.OwnerServiceInterface = &ownerservice.OwnerService{}
 
-	service := ownerservice.NewOwnerServiceWithDependencies(mockGetSquadDetails)
+	mockSquads := createMockSquads()
+	service := ownerservice.NewOwnerServiceWithSquads(mockSquads)
 
 	// Verify the interface method works
 	owner, err := service.GetOwnerByTribeAndSquad("TRIBE FOOBARBZ42", "squad1")
@@ -223,24 +414,49 @@ func TestOwnerService_GetOwnerByTribeAndSquad_Interface(t *testing.T) {
 }
 
 func TestNewOwnerService(t *testing.T) {
-	// Test that NewOwnerService creates a service with default dependencies
+	// Test that NewOwnerService creates a service
 	service := ownerservice.NewOwnerService()
 	assert.NotNil(t, service)
 
-	// The service should be ready to use (though we can't test GetSquadDetails without real data)
+	// The service should be ready to use (though squads map might be empty if YAML file is not found)
 	// This test ensures the constructor works correctly
 }
 
-func TestNewOwnerServiceWithDependencies(t *testing.T) {
-	// Test that dependency injection works
-	mockFunc := func(squadName string) (ownerservice.SquadDetails, error) {
-		return ownerservice.SquadDetails{
-			JiraTeamID: "test-jira-id",
-			Tribe:      "TEST TRIBE",
-		}, nil
+func TestNewOwnerServiceWithSquads(t *testing.T) {
+	// Test that NewOwnerServiceWithSquads works with provided squads
+	mockSquads := map[string]ownerservice.Group{
+		"test-squad": {
+			APIVersion: "backstage.io/v1alpha1",
+			Kind:       "Group",
+			Metadata: struct {
+				Name        string            `yaml:"name"`
+				Description string            `yaml:"description"`
+				Annotations map[string]string `yaml:"annotations"`
+				Links       []struct {
+					Title string `yaml:"title"`
+					URL   string `yaml:"url"`
+					Icon  string `yaml:"icon"`
+					Type  string `yaml:"type"`
+				} `yaml:"links"`
+			}{
+				Name:        "test-squad",
+				Annotations: map[string]string{"jiraTeamID": "test-jira-id"},
+			},
+			Spec: struct {
+				Profile struct {
+					DisplayName string `yaml:"displayName"`
+				} `yaml:"profile"`
+				Type     string   `yaml:"type"`
+				Parent   string   `yaml:"parent"`
+				Children []string `yaml:"children"`
+			}{
+				Type:   "squad",
+				Parent: "TEST TRIBE",
+			},
+		},
 	}
 
-	service := ownerservice.NewOwnerServiceWithDependencies(mockFunc)
+	service := ownerservice.NewOwnerServiceWithSquads(mockSquads)
 	assert.NotNil(t, service)
 
 	owner, err := service.GetOwnerByTribeAndSquad("TEST TRIBE", "test-squad")
